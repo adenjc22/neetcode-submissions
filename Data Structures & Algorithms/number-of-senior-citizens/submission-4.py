5a3c1b4aa664071ca8bd5f5d91cdf9116354c886
Aden Campbell <c3027789@newcastle.ac.uk>
2026-07-14 21:04:42 +0100

    Add: number-of-senior-citizens - submission-4

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        senior = []
+        pointer
+        
+        for i in details:
+            if i != int(details[i]):
+                senior.append(i)
+
+        return senior
